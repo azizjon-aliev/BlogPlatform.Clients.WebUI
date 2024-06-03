c9f4211e3ff278230a05bea399665284eab01a86
Azizjon <azizaliev2337@gmail.com>
2024-06-03 10:17:19 +0500

    use docker file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,21 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/',
 	plugins: [react()],
 	css: {
 		postcss: {
 			plugins: [tailwindcss()],
 		},
+	},
+	preview: {
+		port: 3000,
+		strictPort: true,
+	},
+	server: {
+		port: 3000,
+		strictPort: true,
+		host: true,
+		origin: 'http://0.0.0.0:3000',
 	},
 })
